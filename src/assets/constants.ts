2a2df6e633707a504c63a0c880deb1ea3980f7f8
Matteo Besio <matteo.besio@gmail.com>
2025-02-14 20:52:10 -0500

    fixing prod build

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,1 +1,2 @@
-export const SERVER_DOMAIN = 'http://localhost:3001/api'
+// export const SERVER_DOMAIN = 'http://localhost:3001/api'
+export const SERVER_DOMAIN = 'https://playground.geniumessays.com/api'
